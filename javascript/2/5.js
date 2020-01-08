8b1bc34c59b1b8a1cfacf67b061b382679f4dbaf
Han Kim <joshhk72@gmail.com>
2020-01-07 20:49:23 -0500

    Set up ch2 pr5 in JavaScript

---
+++ javascript/2/5.js
@@ -0,0 +1,59 @@
+var LinkedList = function (value) {
+  this.value = value;
+  this.next = null;
+};
+
+var printList = function (a) {
+  while (a !== null) {
+    console.log(a.value);
+    a = a.next;
+  }
+};
+
+const sumLists = (headOne, headTwo) => {
+
+}
+
+/* TEST */
+
+// Input: (7 -> 1 -> 6) + (5 -> 9 -> 2). this case refers to 617 + 295
+// Output: 2 -> 1 -> 9. the answer refers to 912
+
+var a = new LinkedList(7);
+var b = new LinkedList(1);
+var c = new LinkedList(6);
+
+a.next = b;
+b.next = c;
+
+var d = new LinkedList(5);
+var e = new LinkedList(9);
+var f = new LinkedList(2);
+
+d.next = e;
+e.next = f;
+
+var newHead = sumList(a, d);
+
+printList(newHead);
+
+// Input: (7 -> 1 -> 6) + (5 -> 9 -> 9). this case refers to 617 + 995
+// Output: 2 -> 1 -> 9. the answer refers to 1612
+
+var a = new LinkedList(7);
+var b = new LinkedList(1);
+var c = new LinkedList(6);
+
+a.next = b;
+b.next = c;
+
+var d = new LinkedList(5);
+var e = new LinkedList(9);
+var f = new LinkedList(2);
+
+d.next = e;
+e.next = f;
+
+var newHead = sumList(a, d);
+
+printList(newHead);
