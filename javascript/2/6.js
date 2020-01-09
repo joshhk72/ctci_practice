75b6b35c923418f38676b3de8f2a364cce6e2f29
Han Kim <joshhk72@gmail.com>
2020-01-08 23:19:27 -0500

    Implement basic iterative answer for ch2 pr6

--- javascript/2/6.js
+++ javascript/2/6.js
@@ -5,7 +5,16 @@ var LinkedList = function (value) {
   this.next = null;
 };
 
-
 const palindrome = list => {
+  const stack = [];
+  let current = list;
+  while (current) {
+    stack.push(current.value);
+    current = current.next;
+  }
 
+  for (let i = 0; i < stack.length; i++) {
+    if (stack[i] !== stack[stack.length-1-i]) return false;
+  }
+  return true;
 }
